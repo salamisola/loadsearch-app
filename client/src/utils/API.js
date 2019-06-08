c6221455166a8e4b684ee27791ebfa9e5c8209c6
solasalami <salamisola@gmail.com>
2019-06-08 16:18:31 -0400

    API file updated

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,4 @@
+
 import axios from "axios";
 
 
@@ -9,7 +10,7 @@ export default {
 
   //Get the loads with given ids
   getLoads: function (equipment, distance, rate) {
-    return axios.get("/loads/" + equipment + "/" + distance + "/" + rate);
+    return axios.get("http://localhost:3001/loads/" + equipment + "/" + distance + "/" + rate);
   },
   // Gets the load with the given id
   getLoad: function (id) {
@@ -24,4 +25,33 @@ export default {
     return axios.post("/loads/");
   }
 };
+
+//=================================================
+
+/*import axios from "axios";
+
+
+export default {
+  // Gets all loadss
+  /* getLoads: function () {
+    return axios.get("/api/loads");
+  }, */
+
+  //Get the loads with given ids
+/*getLoads: function (equipment, distance, rate) {
+  return axios.get("/loads/" + equipment + "/" + distance + "/" + rate);
+},
+// Gets the load with the given id
+getLoad: function (id) {
+  return axios.get("/loads/");
+},
+// Deletes the load with the given id
+deleteLoad: function (id) {
+  return axios.delete("/loads/");
+},
+// Saves a load to the database
+saveLoad: function (LoadData) {
+  return axios.post("/loads/");
+}
+};*/
 
