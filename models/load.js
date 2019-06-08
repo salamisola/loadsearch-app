910c8d47d4d7ca4621589a794cc7d709e0b8da6b
solasalami <salamisola@gmail.com>
2019-06-08 14:46:25 -0400

    files updated

--- models/load.js
+++ models/load.js
@@ -5,7 +5,7 @@ const loadSchema = new Schema({
   origin: { type: String, required: true },
   radius: { type: Number, required: true },
   destination: { type: String, required: true },
-  equipement: { type: String, required: true },
+  equipment: { type: String, required: true },
   rate: { type: Number, required: true },
   ratepermile: { type: Number, required: true },
   distance: { type: Number, required: true },
