b03f0ac6acb767d8ad5f800cefa0d5f98e08c10f
solasalami <salamisola@gmail.com>
2019-06-08 15:50:51 -0400

    server file updated

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-const mongoose = require("mongoose");
-const routes = require("./routes/api");
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+const routes = require("./routes/api");
 
 // Add routes, both API and view
 app.use(routes);
@@ -29,3 +50,37 @@ mongoose.connect(
 app.listen(PORT, () =>
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
+
+
+//=========================================================
+/*const mongoose = require("mongoose");
+const routes = require("./routes/api");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+/* app.use(mongo_query); */
+
+// Connect to the Mongo DB
+/*mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/loadsearchdb",
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true
+    }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);*/
